f16a7e38477903740c85f299b5e0f687449a5aa7
Victor Carpenter <victorcarpenter123@gmail.com>
2017-02-02 18:44:10 -0500

    started putting in basic redux before i lay out react

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,11 +1,29 @@
 // console.log('Hello World!');
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Counter from './Counter';
+import { createStore } from 'redux';
+// import Counter from './Counter';
+//
+// document.addEventListener('DOMContentLoaded', function() {
+//   ReactDOM.render(
+//     // React.createElement(Counter),
+//     document.getElementById('mount')
+//   );
+// });
 
-document.addEventListener('DOMContentLoaded', function() {
-  ReactDOM.render(
-    React.createElement(Counter),
-    document.getElementById('mount')
-  );
-});
+
+const reducer = function(state, action) {
+  let newState = state;
+  ? action.type === "NAME_CHANGE" : () => {
+
+  }
+}
+
+
+const store = createStore(reducer, {name: 'fred', age: 35});
+
+store.subscribe( () => {
+  console.log("here be the store", store.getState())
+})
+
+store.dispatch({tpye: "NAME_CHANGE", payload: 'will'})
