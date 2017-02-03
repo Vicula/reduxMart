de2211e6e73f35cba952946f3f739901eacd5547
Victor Carpenter <victorcarpenter123@gmail.com>
2017-02-02 20:15:48 -0500

    making the basic layout for the user reducer

---
+++ src/scripts/reducers/userRed.js
@@ -0,0 +1,16 @@
+
+var newUser = {
+  username: '',
+  age: '',
+  desc: ''
+}
+
+var userStore = {
+  users: []
+}
+
+
+const userReducer = (store=(...userStore), actions) => {
+  let newStore = store;
+  return newStore
+}
